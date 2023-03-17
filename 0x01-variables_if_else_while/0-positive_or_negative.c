ea77b7f877ec35658551d86b268da5c249cd30a4
HanRachid <handaouirachid@gmail.com>
2023-03-17 12:36:48 -0700

    add README.md

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/** 
+ * main function containing algorithms to check random number's sign
+ */
 int main(void)
 {
 	int n;
@@ -12,11 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive",n);
+		printf("%d is positive", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative",n);
+		printf("%d is negative", n);
 	}
 	else
 	{
