c3087f3d3d0de281c302bb03b0876ed0bcb6b96a
HanRachid <handaouirachid@gmail.com>
2023-03-17 12:48:02 -0700

    add README.md

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
+/**
  * main - function containing algorithms to check random number's sign
+ *
  * Return : returns 0, always a success
  */
 int main(void)
