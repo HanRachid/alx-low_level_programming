9fe3b0e02401420bc01d47126689d7899bb91433
HanRachid <handaouirachid@gmail.com>
2023-03-21 17:12:22 -0700

    correct ex 9 and 8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar('0' + number / 10);
 				_putchar('0' + number % 10);
 				if (m < 9)
-					_putchar(',')
+					_putchar(',');
 			}
 			else
 			{
@@ -31,7 +31,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar('0' + number);
 				if (m < 9)
-					_putchar(',')
+					_putchar(',');
 			}
 		}
 		_putchar('\n');
