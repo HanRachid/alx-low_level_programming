3af8abb36fb14427245492c1eba3686b751732a1
HanRachid <handaouirachid@gmail.com>
2023-03-21 17:11:40 -0700

    correct ex 9 and 8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,42 +6,33 @@
  */
 void times_table(void)
 {
-	int h; int m; int number;
+	int h;
+	int m;
 
 	for (h = 0; h < 10; h++)
 	{
 		_putchar('0');
 		_putchar(',');
-		for (m = 1; m < 9; m++)
+		for (m = 1; m < 10; m++)
 		{
-			number = h * m;
+			int number = h * m;
 
 			if (number / 10 > 0)
 			{
 				_putchar(' ');
 				_putchar('0' + number / 10);
 				_putchar('0' + number % 10);
-				_putchar(',');
+				if (m < 9)
+					_putchar(',')
 			}
 			else
 			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar('0' + number);
-				_putchar(',');
+				if (m < 9)
+					_putchar(',')
 			}
-		}
-		if ((number + h) / 10 > 0)
-		{
-			_putchar(' ');
-			_putchar('0' + (number + h) / 10);
-			_putchar('0' + (number + h) % 10);
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + (number + h));
 		}
 		_putchar('\n');
 	}
