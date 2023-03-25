48991463e9478b8551f2f326dc34565b17434191
HanRachid <handaouirachid@gmail.com>
2023-03-24 17:58:20 -0700

    add ex 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints biggest prime factor of 612852475143
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int i = 2;
+	long int currentfactor = 1;
+	long int factorized = 612852475143;
+
+	while (i < factorized / currentfactor)
+	{
+		i++;
+		if (factorized % i == 0)
+		{
+			currentfactor = i;
+		}
+	}
+	printf("%ld", i);
+}
