033d3044df3fc7ffb7b75df61c2b86b7979ea98f
HanRachid <handaouirachid@gmail.com>
2023-03-26 22:01:34 -0700

    add ex 2 and 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints char until \0 char is encountered
+ * @s: pointer to the first element of a string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	char *n = s;
+
+	while (*n != '\0')
+	{
+		_putchar(*n);
+		n++;
+	}
+	_putchar('\n');
+}
