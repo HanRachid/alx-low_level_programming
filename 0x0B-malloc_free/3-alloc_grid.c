ebf88474546fc3c6a4ed6f34435a745f42cabd24
HanRachid <handaouirachid@gmail.com>
2023-04-11 11:28:35 -0700

    add ex 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,13 +14,13 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width < 0 || height < 0)
-		return NULL;
+		return (NULL);
 
-	arrptr = malloc(sizeof(int) * width);
+	arrptr = malloc(sizeof(int *) * height);
 
 	for (i = 0; i < height; i++)
 	{
-		arrptr[i] = malloc(sizeof(int) * height);
+		arrptr[i] = malloc(sizeof(int) * width);
 	}
 
 	for (i = 0; i < height; i++)
