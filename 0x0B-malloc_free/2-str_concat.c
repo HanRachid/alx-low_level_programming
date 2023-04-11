94cca054023f1c13bf528f62329eb358a35febdb
HanRachid <handaouirachid@gmail.com>
2023-04-11 00:59:05 -0700

    add ex01

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * _strdup - creates pointer to array and concatenates
- * string from parameters into it
- * @s1: pointer to first string
- * @s2: pointer to second string
- * Return: pointer to new array
- */
 char *str_concat(char *s1, char *s2)
 {
-	int size1;
-	int size2;
-	char *ptr1;
-	char *ptr2;
-	char *arr;
-	int i = 0;
+	int size1 = 0, size2 = 0, i = 0;
+	char *ptr1 = "", *ptr2 = "", *arr;
 
-	ptr2 = s2;
-
-	if (s1 == NULL)
-	{
-		ptr1 = "";
-		size1 = 0;
-	}
-	else
+	if (s1 != NULL)
 	{
 		ptr1 = s1;
 		size1 = strlen(s1);
 	}
-	if (s2 == NULL)
-	{
-		ptr2 = "";
-		size2 = 0;
-	}
-	else
+	if (s2 != NULL)
 	{
 		ptr2 = s2;
 		size2 = strlen(s2);
@@ -46,12 +19,10 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	while (*ptr1 != '\0')
 	{
-
 		arr[i++] = *ptr1++;
 	}
 	while (*ptr2 != '\0')
 	{
-
 		arr[i++] = *ptr2++;
 	}
 	arr[i++] = '\0';
