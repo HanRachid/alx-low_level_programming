fc72f42f48b82d988ceff98572a037f0647824fb
HanRachid <handaouirachid@gmail.com>
2023-04-17 18:56:57 -0700

    add ex 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,9 +17,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc((sizeof(dog_t)));
 	if (!dog)
 		return (NULL);
-	dog->name = name;
+	dog->name = strdup(name);
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = strdup(owner);
 
 	return (dog);
 }
