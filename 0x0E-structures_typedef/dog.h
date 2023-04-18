0afd5a5c546c4a1c5ce935216bb8bc90969e0f3d
HanRachid <handaouirachid@gmail.com>
2023-04-17 17:49:44 -0700

    start 0X0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
+
