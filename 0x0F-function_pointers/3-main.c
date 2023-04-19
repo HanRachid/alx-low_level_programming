0fb976f3ba2a731aab995ee52247d3639ae95650
HanRachid <handaouirachid@gmail.com>
2023-04-19 10:58:35 -0700

    fix main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,17 +15,17 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(98);
 	}
 	if (!get_op_func(argv[2]))
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
 	if ((*argv[2] == '%' || *argv[2] == '/') && atoi(argv[3]) == 0)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(100);
 	}
 	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
