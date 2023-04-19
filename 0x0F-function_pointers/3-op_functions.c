cae88884ae1738e54f115ca43c0da763533c1bda
HanRachid <handaouirachid@gmail.com>
2023-04-19 11:07:06 -0700

    fix main

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,7 @@
-#include "3-calc.h"
+#include "calc.h"
 
 /**
- * op_add - prints index of element that satisfied cmp
+ * op_add - adds a and b
  * @a: first int
  * @b: second int
  * Return: sum of a and b
@@ -11,7 +11,7 @@ int op_add(int a, int b)
 	return (a + b);
 }
 /**
- * op_sub - prints index of element that satisfied cmp
+ * op_sub - subtracts a and b
  * @a: first int
  * @b: second int
  * Return: substraction of a and b
@@ -21,7 +21,7 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 /**
- * op_mul - prints index of element that satisfied cmp
+ * op_mul - multiplies a and b
  * @a: first int
  * @b: second int
  * Return: a multiplied by b
@@ -31,7 +31,7 @@ int op_mul(int a, int b)
 	return (a * b);
 }
 /**
- * op_div - prints index of element that satisfied cmp
+ * op_div - divides a by b
  * @a: first int
  * @b: second int
  * Return: division of a and b
@@ -42,7 +42,7 @@ int op_div(int a, int b)
 	return (a / b);
 }
 /**
- * op_mod - prints index of element that satisfied cmp
+ * op_mod - a modulo b
  * @a: first int
  * @b: second int
  * Return: modulo of a and b
