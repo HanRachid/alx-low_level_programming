d1c7a28d2a389cb6c30c3af5b458a724593a8bbd
HanRachid <handaouirachid@gmail.com>
2023-04-27 15:50:27 -0700

    start linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_numbers - prints all numbers
+ * @separator: separator
+ * @h : pointer to list_t instance
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodecount = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		nodecount++;
+	}
+
+	return (nodecount);
+}
+
