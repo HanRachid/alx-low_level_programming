2f74e9aec8fa309d9248a06e055dd36a80d0e875
HanRachid <handaouirachid@gmail.com>
2023-04-27 16:11:06 -0700

    start linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,48 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
+/**
+ * prints - prints string
+ * @str : pointer to string
+ * Return: void
+ */
+
+void prints(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
+
+/**
+ * putnbr- prints number
+ * @nb : number
+ * Return: void
+ */
+
+void putnbr(int nb)
+{
+	int tempnum = nb;
+	char c;
+	int i = 1;
+
+	while (tempnum > 9)
+	{
+		tempnum = tempnum / 10;
+		i *= 10;
+	}
+
+	c = '0' + tempnum;
+	_putchar(c);
+	if (nb - tempnum * i != 0)
+	{
+		putnbr(nb - tempnum * i);
+	}
+}
+
 /**
  * print_list - prints list according to format
  * @h : pointer to list_t instance
