bb4e85ac92113c26964f9e63b192e44123f2dd72
HanRachid <handaouirachid@gmail.com>
2023-05-25 21:14:36 +0100

    add exo 2 é

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,14 +22,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	i = read(fd, buf, letters);
 	if (i == -1)
 	{
@@ -37,14 +35,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	buf[i] = '\0';
-
 	n = write(STDOUT_FILENO, buf, i);
 	if (n == -1)
 	{
 		free(buf);
 		return (0);
 	}
-
 	close(fd);
 	if (fd == -1)
 	{
