84c3684785457e852b3998fce050090cfb1f13dd
HanRachid <handaouirachid@gmail.com>
2023-09-07 21:22:52 +0100

    fix 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,16 +10,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *current;
-	int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *node;
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-	current = (hash_node_t *)calloc(ht->size, sizeof(hash_node_t));
-	if (ht->array[index] == NULL)
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
 		return (NULL);
-	current = ht->array[index];
-	while (current && strcmp(current->key, key) != 0)
-		current = current->next;
-	return ((current == NULL) ? NULL : current->value);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
