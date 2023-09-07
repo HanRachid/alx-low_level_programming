5a6536d2858a2b9814080893b703bba42a081230
HanRachid <handaouirachid@gmail.com>
2023-09-07 12:12:31 +0100

    start hash tables day

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,39 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creating hash table.
- * @size: Aray size.
- *
- * Return: If an error occurs - NULL.
- *	   Else - pointer to new hash table.
+ * hash_table_create - Creates hash table
+ * @size: size of array
+ * Return: the created hashtable
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+	int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+	hash_table_t *htable = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-	return (ht);
+	htable->size = size;
+	htable->array = (hash_node_t **)calloc(htable->size, sizeof(hash_node_t *));
+
+	for (i = 0; i < htable->size; i++)
+	{
+		htable->array[i] = NULL;
+	}
+	return (htable);
+}
+/**
+ * hash_function - generates key from value
+ * @value: value to turn into hash key
+ * @size: limit of value of key
+ * Return: generated hashkey
+ */
+int hash_function(char *value, int size)
+{
+	int key = 0;
+
+	while (*value != '\0')
+	{
+		key += *value++;
+	}
+	return (key % size);
 }
